c73820578c00ff1d4680c6171303b689bcd89f3c
Ruben Martinez Jr <rubencodes@users.noreply.github.com>
2021-01-13 11:42:19 -0500

    Update util.js

--- build/util.js
+++ build/util.js
@@ -16,15 +16,21 @@ function makeTask(opts) {
 
   function runTask() {
     // Gather the files from the entry directory (dir)
-    src(opts.src.map(file => config.srcDir + file), {
-      sourcemaps: config.sourcemaps
-    }).pipe(dest(config.distDir)); // Copy to the Dist dir
+    src(
+      opts.src.map((file) => config.srcDir + file),
+      {
+        sourcemaps: config.sourcemaps
+      }
+    ).pipe(dest(config.distDir)); // Copy to the Dist dir
 
-    const filesToModify = src(opts.src.map(file => config.distDir + file), {
-      sourcemaps: config.sourcemaps
-    }); // Make sure we include any additional files from the dist dir
+    const filesToModify = src(
+      opts.src.map((file) => config.distDir + file),
+      {
+        sourcemaps: config.sourcemaps
+      }
+    ); // Make sure we include any additional files from the dist dir
 
-    const pipes = pipe.map(processor => {
+    const pipes = pipe.map((processor) => {
       const fn = require(processor.require);
       return fn.apply(null, processor.args);
     });
@@ -64,7 +70,7 @@ function makeTask(opts) {
 function makeRename(opts) {
   // Find & replace of the basename if `find` option passed in
   if (opts.find) {
-    return rename(function(path) {
+    return rename(function (path) {
       path.basename = path.basename.replace(opts.find, opts.replace);
     });
   }
@@ -78,7 +84,7 @@ function makeRename(opts) {
 function makeCleanup(opts) {
   function runCleanup(done) {
     const filesToDelete = Array.isArray(opts.cleanup) ? opts.cleanup : opts.src;
-    del.sync(filesToDelete.map(file => config.distDir + file));
+    del.sync(filesToDelete.map((file) => config.distDir + file));
     done();
   }
   runCleanup.displayName = 'cleanup:' + opts.key;
